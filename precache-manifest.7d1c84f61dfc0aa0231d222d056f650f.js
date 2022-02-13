e1eaaedb872bcb8b4e7343a3451e5d0a81af22ac
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-02-14 00:17:01 +0100

    Updates

---
+++ precache-manifest.7d1c84f61dfc0aa0231d222d056f650f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dafbab6a16262b6de1f67becc05d6e",
+    "url": "/faceRecognitionApp/index.html"
+  },
+  {
+    "revision": "0ac32c19a113a1f6836b",
+    "url": "/faceRecognitionApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "81e81fee0b9ba39d1a10",
+    "url": "/faceRecognitionApp/static/css/main.e5f0b50f.chunk.css"
+  },
+  {
+    "revision": "0ac32c19a113a1f6836b",
+    "url": "/faceRecognitionApp/static/js/2.84a5bac8.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/faceRecognitionApp/static/js/2.84a5bac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e81fee0b9ba39d1a10",
+    "url": "/faceRecognitionApp/static/js/main.f7fa6c21.chunk.js"
+  },
+  {
+    "revision": "e8b5dafad751cbd2b223",
+    "url": "/faceRecognitionApp/static/js/runtime-main.bb29a385.js"
+  },
+  {
+    "revision": "87466cd4f606eee3a29e9b9a78ef517a",
+    "url": "/faceRecognitionApp/static/media/brain-icon.87466cd4.png"
+  }
+]);
